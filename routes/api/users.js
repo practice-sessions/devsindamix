535850839ee95398667d9f6981733a63d5e8be24
hixsav <hi.savis@gmail.com>
2018-05-14 10:52:14 +0100

    Updated files

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,4 +6,9 @@ const router = express.Router();
 // @access   Public
 router.get('/test', (req, res) => res.jsonp({ msg: "Users works" }));
 
+// @route   GET api/users/test
+// @desc    Tests user route
+// @access   Public
+router.
+
 module.exports = router;
