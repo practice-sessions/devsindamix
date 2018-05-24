ec79e1652e4d5a15d7940db1800b5acd5a057db6
hixsav <hi.savis@gmail.com>
2018-05-24 17:52:26 +0100

    Updated posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Post model
 const Post = require('../../models/Post');
 
+// Profile model
+const Profile = require('../../models/Profile');
+
 // Validation
 const validatePostInput = require('../../validation/post');
 
@@ -13,6 +16,22 @@ const validatePostInput = require('../../validation/post');
 //@desc    Tests posts route
 //@access   Public
 router.get('/test', (req, res) => res.jsonp({ msg: "Posts works" }));
+
+//@route   GET api/posts       <All posts>
+//@desc    get posts
+//@access  Public
+router.get('/', (req, res) => {
+  Post.find().sort({ date: -1 }).then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+//@route   GET api/posts/:id        <Single posts>
+//@desc    Get post by id
+//@access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id).then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: 'No post found for this user' }));
+});
 
 //@route    POST api/posts
 //@desc     Create post
@@ -37,6 +56,26 @@ router.post('/', passport.authenticate(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+//@route   DELETE api/posts/:id       <Single posts>
+//@desc    Delete post
+//@access  Private - reflects protected route status
+router.delete('/:id', passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      // Check for post, prevent malicious deletion
+      if (post.user.toString !== req.user.id) {
+
+        return res.status(401)
+          .json({ notauthorized: 'User not authorised' });
+      }
+
+
+      // Delete                                           
+      post.remove().then(() => res.json({ success: 'true' }));
+    }).catch(err => res.status(404).json({ postnotfound: 'No post found' }));
   }
 );
 
